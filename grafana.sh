bd97afd01a4df826665934f840890c0cc4998dc7
Nari0417 <narayanareddy9965@gmail.com>
2025-03-14 18:05:33 +0530

    Create grafana.sh

---
+++ grafana.sh
@@ -0,0 +1,12 @@
+sudo nano /etc/yum.repos.d/grafana.repo
+#Add the text below to the repo file. This will install the open-source Grafana.
+
+[grafana]
+name=grafana
+baseurl=https://packages.grafana.com/oss/rpm
+repo_gpgcheck=1
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.grafana.com/gpg.key
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
