89106ea563ac559a04484a0efe2e34363713d672
Nari0417 <narayanareddy9965@gmail.com>
2025-03-14 18:06:57 +0530

    Update grafana.sh

--- grafana.sh
+++ grafana.sh
@@ -1,5 +1,5 @@
-sudo nano /etc/yum.repos.d/grafana.repo
 #Add the text below to the repo file. This will install the open-source Grafana.
+sudo nano /etc/yum.repos.d/grafana.repo
 
 [grafana]
 name=grafana
@@ -10,3 +10,11 @@ gpgcheck=1
 gpgkey=https://packages.grafana.com/gpg.key
 sslverify=1
 sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+===========================================================================================================
+sudo apt-get install -y adduser libfontconfig1
+wget https://dl.grafana.com/enterprise/release/grafana-enterprise_9.4.7_amd64.deb
+sudo dpkg -i grafana-enterprise_9.4.7_amd64.deb
+sudo /bin/systemctl daemon-reload
+sudo /bin/systemctl enable grafana-server
+sudo /bin/systemctl start grafana-server
+sudo /bin/systemctl status grafana-server --no-pager
